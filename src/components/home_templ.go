2668bb7ace8bc24beb75eec170b61f90c592389a
Bell Dixon <bell.dixon@proton.me>
2025-06-26 23:29:03 -0500

    Moving air config for easier usage with git and air

--- src/components/home_templ.go
+++ src/components/home_templ.go
@@ -36,7 +36,7 @@ func Home(banner string) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(banner)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `components/home.templ`, Line: 4, Col: 51}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `src/components/home.templ`, Line: 4, Col: 51}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
